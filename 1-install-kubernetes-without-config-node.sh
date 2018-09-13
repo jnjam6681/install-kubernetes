8920601f251c110a7eacb04ecfd3ffd3c9e34fb6
Unknown <jnjam_pk6681@hotmail.com>
2018-09-13 15:57:21 +0700

    chmod file and remove helm

--- 1-install-kubernetes-without-config-node.sh
+++ 1-install-kubernetes-without-config-node.sh
@@ -36,3 +36,4 @@ exit
 wget https://storage.googleapis.com/kubernetes-helm/helm-${HELM_VERSION}-linux-amd64.tar.gz
 tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz
 sudo mv linux-amd64/helm /usr/local/bin/helm
+rm -rf helm-${HELM_VERSION}-linux-amd64.tar.gz
