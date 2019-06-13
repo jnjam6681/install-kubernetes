bba56854da222f13b5d8e048fbed7c859ef1fcd1
Unknown <jnjam_pk6681@hotmail.com>
2019-06-13 15:05:25 +0700

    Update network

--- 8-ha-cluster-with-kubeadm-v14.sh
+++ 8-ha-cluster-with-kubeadm-v14.sh
@@ -11,15 +11,14 @@ apiVersion: kubeadm.k8s.io/v1beta1
 kind: ClusterConfiguration
 kubernetesVersion: stable
 controlPlaneEndpoint: "${LOAD_BALANCER_DNS}:${LOAD_BALANCER_PORT}"
+networking:
+  podSubnet: 10.244.0.0/16
 EOF
 echo 'create file success.'
 
 # Initialize the control plane
 echo 'kubeadm init'
 sudo kubeadm init --config=kubeadm-config.yaml --experimental-upload-certs
-
-# Initialize network
-kubectl apply -f https://docs.projectcalico.org/v3.7/manifests/canal.yaml
 
 # start cluster
 echo 'start cluster'
@@ -28,5 +27,9 @@ sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
 sudo chown $(id -u):$(id -g) $HOME/.kube/config
 echo 'success.'
 
-# re-upload the certificates and generate a new decryption key 
+# Initialize network
+kubectl apply -f https://docs.projectcalico.org/v3.7/manifests/canal.yaml
+# kubectl apply -f https://docs.projectcalico.org/v3.7/manifests/calico.yaml
+
+# re-upload the certificates and generate a new decryption key
 # sudo kubeadm init phase upload-certs --experimental-upload-certs
