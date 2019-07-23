4ab89b3bfe09f0efe5ae8a0f038b131fa30a48d9
jnjam6681 <jnjam_pk6681@hotmail.com>
2019-07-23 18:53:06 +0700

    Update 1-install-docker.sh

--- 1-install-docker.sh
+++ 1-install-docker.sh
@@ -10,24 +10,24 @@ DOCKER_COMPOSE_VERSION="1.24.0"
 
 # install docker ce
 echo 'install docker-ce'
-apt-get update
-apt-get install -y \
+sudo apt-get update
+sudo apt-get install -y \
   apt-transport-https \
   ca-certificates \
   curl \
   software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-add-apt-repository \
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
   stable"
-apt-get update
-apt-get install -y docker-ce
+sudo apt-get update
+sudo apt-get install -y docker-ce
 # apt-get install -y docker-ce=18.06.1~ce~3-0~ubuntu
 echo 'install docker-ce finish !!'
 
 # install docker compose
 echo 'install docker-compose'
-curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
 echo 'install docker-compose finish !!'
