e8c4a4a5999724cc4e6f21662452cf3e08f6d0ce
jnjam6681 <jnjam_pk6681@hotmail.com>
2019-07-23 19:11:54 +0700

    Update 1-install-docker.sh

--- 1-install-docker.sh
+++ 1-install-docker.sh
@@ -16,7 +16,7 @@ sudo apt-get install -y \
   ca-certificates \
   curl \
   software-properties-common
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository \
   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) \
