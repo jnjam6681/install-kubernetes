85bdbeec9a3caab536773a787af7c1799ea0e1f3
Unknown <jnjam_pk6681@hotmail.com>
2019-08-19 15:54:59 +0700

    Update helm install :beer:

--- 4-install-helm-for-master.sh
+++ 4-install-helm-for-master.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-HELM_VERSION="v2.14.2"
+HELM_VERSION="v2.14.3"
 # install helm
 echo 'install helm'
 wget https://storage.googleapis.com/kubernetes-helm/helm-${HELM_VERSION}-linux-amd64.tar.gz
@@ -7,4 +7,10 @@ tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
 rm -rf helm-${HELM_VERSION}-linux-amd64.tar.gz
 rm -rf linux-amd64
+
+kubectl -n kube-system create serviceaccount tiller
+kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
+helm init --service-account tiller
+
+helm upgrade repo
 echo 'install helm finish !!'
