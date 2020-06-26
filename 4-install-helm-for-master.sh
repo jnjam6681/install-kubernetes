56ffc5086fa465b19192e604c0982ed057589f43
jnjam6681 <jnjam_pk6681@hotmail.com>
2020-06-26 15:53:37 +0700

    Update 4-install-helm-for-master.sh

--- 4-install-helm-for-master.sh
+++ 4-install-helm-for-master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-HELM_VERSION="v2.14.3"
+HELM_VERSION="v2.16.9"
 # install helm
 echo 'install helm'
-wget https://storage.googleapis.com/kubernetes-helm/helm-${HELM_VERSION}-linux-amd64.tar.gz
+wget https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz
 tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz
 mv linux-amd64/helm /usr/local/bin/helm
 rm -rf helm-${HELM_VERSION}-linux-amd64.tar.gz
