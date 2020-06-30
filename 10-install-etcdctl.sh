4abf5919581c9a0740134dcc56a535ed14d71201
assassinsilence <jn13122537>
2020-06-30 10:27:40 +0700

    🍣install etcdctl

---
+++ 10-install-etcdctl.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+ETCD_VER=v3.4.9
+
+# install etcdctl
+echo 'install etcdctl'
+curl -L https://storage.googleapis.com/etcd/${ETCD_VER}/etcd-${ETCD_VER}-linux-amd64.tar.gz -o /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+tar xzvf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz 
+mv /tmp/etcd-${ETCD_VER}-linux-amd64/etcdctl /usr/local/bin/etcdctl
+rm -rf /tmp/etcd-${ETCD_VER}-linux-amd64.tar.gz
+rm -rf /tmp/etcd-${ETCD_VER}-linux-amd64
